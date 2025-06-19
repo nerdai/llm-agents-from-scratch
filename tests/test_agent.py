129a29314c324e40e9121e52760bf7275ff8abac
nerdai <andrei@nerdai.io>
2025-06-19 02:13:46 -0400

    add tool test

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,3 +1,5 @@
+from unittest.mock import MagicMock
+
 from llm_agents_from_scratch.base.llm import BaseLLM
 from llm_agents_from_scratch.core import LLMAgent
 
@@ -9,3 +11,17 @@ def test_init(mock_llm: BaseLLM) -> None:
 
     assert len(agent.tools) == 0
     assert agent.llm == mock_llm
+
+
+def test_add_tool(mock_llm: BaseLLM) -> None:
+    """tests add tool"""
+
+    # arrange
+    tool = MagicMock()
+    agent = LLMAgent(llm=mock_llm)
+
+    # act
+    agent.add_tool(tool)
+
+    # assert
+    assert agent.tools == [tool]

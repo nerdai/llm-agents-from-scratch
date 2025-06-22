e838aa87bfa448d305cb68fae029a0742aaca62b
nerdai <andrei@nerdai.io>
2025-06-22 17:09:55 -0400

    unit test for exception

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -13,20 +13,6 @@ from llm_agents_from_scratch.data_structures.agent import (
 )
 
 
-class MockTaskHandler(TaskHandler):
-    @override
-    async def get_next_step(self) -> TaskStep | None:
-        await asyncio.sleep(0.1)
-        return TaskStep(instruction="mock step")
-
-    @override
-    async def run_step(self, step: TaskStep) -> TaskStepResult:
-        await asyncio.sleep(0.1)
-        return TaskStepResult(
-            task_step=step, content="mock result", last_step=True
-        )
-
-
 def test_init(mock_llm: BaseLLM) -> None:
     """Tests init of LLMAgent."""
 
@@ -57,6 +43,19 @@ async def test_run(
 ) -> None:
     """Tests run method."""
 
+    class MockTaskHandler(TaskHandler):
+        @override
+        async def get_next_step(self) -> TaskStep | None:
+            await asyncio.sleep(0.1)
+            return TaskStep(instruction="mock step")
+
+        @override
+        async def run_step(self, step: TaskStep) -> TaskStepResult:
+            await asyncio.sleep(0.1)
+            return TaskStepResult(
+                task_step=step, content="mock result", last_step=True
+            )
+
     # arrange
     agent = LLMAgent(llm=mock_llm)
     task = Task(instruction="mock instruction")
@@ -74,3 +73,32 @@ async def test_run(
     assert handler == mock_handler
     mock_task_handler_class.assert_called_once_with(task)
     assert handler.result().content == "mock result"
+
+
+@pytest.mark.asyncio
+@patch("llm_agents_from_scratch.core.agent.TaskHandler")
+async def test_run_exception(
+    mock_task_handler_class: MagicMock, mock_llm: BaseLLM
+) -> None:
+    """Tests run method with exception."""
+
+    err = RuntimeError("mock error")
+
+    class MockTaskHandler(TaskHandler):
+        @override
+        async def get_next_step(self) -> TaskStep | None:
+            raise err
+
+    # arrange
+    agent = LLMAgent(llm=mock_llm)
+    task = Task(instruction="mock instruction")
+    mock_handler = MockTaskHandler(task)
+    mock_task_handler_class.return_value = mock_handler
+
+    # act
+    handler = agent.run(task)
+    await asyncio.sleep(0.1)  # Let it run
+
+    assert handler == mock_handler
+    mock_task_handler_class.assert_called_once_with(task)
+    assert handler.exception() == err

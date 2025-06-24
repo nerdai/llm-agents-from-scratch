e849a8b2bd39778c9adafa10c29556047030c0be
nerdai <andrei@nerdai.io>
2025-06-24 01:17:34 -0400

    ollama dep and tools param to chat method

--- src/llm_agents_from_scratch/base/llm.py
+++ src/llm_agents_from_scratch/base/llm.py
@@ -4,6 +4,8 @@ from abc import ABC, abstractmethod
 
 from llm_agents_from_scratch.data_structures import ChatMessage, CompleteResult
 
+from .tool import BaseTool
+
 
 class BaseLLM(ABC):
     """Base LLM Class."""
@@ -20,11 +22,14 @@ class BaseLLM(ABC):
         """
 
     @abstractmethod
-    async def chat(self, chat_messages: list[ChatMessage]) -> ChatMessage:
+    async def chat(
+        self, chat_messages: list[ChatMessage], tools: list[BaseTool] = []
+    ) -> ChatMessage:
         """Chat interface.
 
         Args:
             chat_messages (list[ChatMessage]): chat history.
+            tools (list[BaseTool]): tools that the LLM can call.
 
         Returns:
             ChatMessage: The response of the LLM structured as a `ChatMessage`.
--- tests/llm/test_base.py
+++ tests/llm/test_base.py
@@ -2,6 +2,7 @@ from llm_agents_from_scratch.base.llm import BaseLLM
 
 
 def test_base_abstract_attr() -> None:
+    """Tests abstract methods in base class."""
     abstract_methods = BaseLLM.__abstractmethods__
 
     assert "complete" in abstract_methods

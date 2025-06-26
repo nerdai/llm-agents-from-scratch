83cf5974b738caa15f55c5c37bfd589464946b09
nerdai <andrei@nerdai.io>
2025-06-26 02:11:50 -0400

    more cover

--- tests/llms/test_ollama.py
+++ tests/llms/test_ollama.py
@@ -19,6 +19,10 @@ def test_chat_message_to_ollama_message() -> None:
     """Tests conversion from ChatMessage to ~ollama.Message."""
     messages = [
         ChatMessage(
+            role="system",
+            content="0",
+        ),
+        ChatMessage(
             role="user",
             content="1",
         ),
@@ -30,15 +34,21 @@ def test_chat_message_to_ollama_message() -> None:
 
     ollama_messages = [chat_message_to_ollama_message(m) for m in messages]
 
-    assert ollama_messages[0].content == "1"
-    assert ollama_messages[0].role == "user"
-    assert ollama_messages[1].content == "2"
-    assert ollama_messages[1].role == "assistant"
+    assert ollama_messages[0].content == "0"
+    assert ollama_messages[0].role == "system"
+    assert ollama_messages[1].content == "1"
+    assert ollama_messages[1].role == "user"
+    assert ollama_messages[2].content == "2"
+    assert ollama_messages[2].role == "assistant"
 
 
 def test_ollama_message_to_chat_message() -> None:
     """Tests conversion from ~ollama.Message to ChatMessage."""
     messages = [
+        OllamaMessage(
+            role="system",
+            content="0",
+        ),
         OllamaMessage(
             role="user",
             content="1",
@@ -66,9 +76,11 @@ def test_ollama_message_to_chat_message() -> None:
 
     converted = [ollama_message_to_chat_message(m) for m in messages]
 
-    assert converted[0].role == ChatRole.USER
-    assert converted[0].content == "1"
-    assert converted[1].role == ChatRole.ASSISTANT
-    assert converted[1].content == "2"
-    assert converted[2].role == ChatRole.TOOL
-    assert converted[2].content == "3"
+    assert converted[0].role == ChatRole.SYSTEM
+    assert converted[0].content == "0"
+    assert converted[1].role == ChatRole.USER
+    assert converted[1].content == "1"
+    assert converted[2].role == ChatRole.ASSISTANT
+    assert converted[2].content == "2"
+    assert converted[3].role == ChatRole.TOOL
+    assert converted[3].content == "3"

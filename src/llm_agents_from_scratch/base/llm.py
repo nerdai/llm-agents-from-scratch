3eb5250271814fbefc0f892a8467d0ecbf73dd92
nerdai <andrei@nerdai.io>
2025-06-27 02:26:10 -0400

    update MockLLM

--- src/llm_agents_from_scratch/base/llm.py
+++ src/llm_agents_from_scratch/base/llm.py
@@ -31,16 +31,17 @@ class BaseLLM(ABC):
     async def chat(
         self,
         input: str,
-        chat_messages: list[ChatMessage] | None = None,
-        tools: list[BaseTool] | None = None,
+        chat_messages: Sequence[ChatMessage] | None = None,
+        tools: Sequence[BaseTool] | None = None,
         **kwargs: Any,
     ) -> ChatMessage:
         """Chat interface.
 
         Args:
             input (str): The user's current input.
-            chat_messages (list[ChatMessage]|None, optional): chat history.
-            tools (list[BaseTool]|None, optional): tools that the LLM can call.
+            chat_messages (Sequence[ChatMessage]|None, optional): chat history.
+            tools (Sequence[BaseTool]|None, optional): tools that the LLM
+                can call.
             **kwargs (Any): Additional keyword arguments.
 
         Returns:
@@ -50,15 +51,15 @@ class BaseLLM(ABC):
     @abstractmethod
     async def continue_conversation_with_tool_results(
         self,
-        tool_call_results: ToolCallResult | Sequence[ToolCallResult],
+        tool_call_results: Sequence[ToolCallResult],
         chat_messages: Sequence[ChatMessage],
         **kwargs: Any,
     ) -> ChatMessage:
         """Continue a conversation submitting tool call results.
 
         Args:
-            tool_call_results (ToolCallResult | Sequence[ToolCallResult]):
-                Tool call results. Can be a single one or a sequence of them.
+            tool_call_results (Sequence[ToolCallResult]):
+                Tool call results.
             chat_messages (Sequence[ChatMessage]): The chat history.
                 Defaults to None.
             **kwargs (Any): Additional keyword arguments.

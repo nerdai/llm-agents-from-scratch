302f02ed62003935f19d144887919c6b1a8c7d77
nerdai <andrei@nerdai.io>
2025-06-29 23:32:46 -0400

    simple mapping

--- src/llm_agents_from_scratch/tools/function.py
+++ src/llm_agents_from_scratch/tools/function.py
@@ -1,9 +1,65 @@
 """Function Tool."""
 
-from typing import Any, Callable
+import inspect
+from typing import Any, Callable, get_type_hints
 
 from llm_agents_from_scratch.base.tool import BaseTool
 from llm_agents_from_scratch.data_structures import ToolCall, ToolCallResult
+
+
+def function_signature_to_json_schema(func: Callable) -> dict[str, Any]:
+    """Turn a function signature into a JSON schema.
+
+    Inspects the signature of the function and maps types to the appropriate
+    JSON schema type.
+
+    Args:
+        func (Callable): The function for which to get the JSON schema.
+
+    Returns:
+        dict[str, Any]: The JSON schema
+    """
+    sig = inspect.signature(func)
+    type_hints = get_type_hints(func)  # resolves forward references
+    python_to_json_schema_type = {
+        str: "string",
+        int: "number",
+        float: "number",
+        dict: "object",
+        list: "array",
+        type(None): "null",
+        bool: "boolean",
+    }
+
+    properties = {}
+    required = []
+    for param in sig.parameters.values():
+        # skip args and kwargs
+        if param.name.startswith(("args", "kwargs")):
+            continue
+
+        # get type annotations
+        annotation = type_hints.get(param.name, param.annotation)
+
+        # get json schema
+        if annotation in python_to_json_schema_type:
+            this_params_json_schema = {
+                "type": python_to_json_schema_type[annotation],
+            }
+        else:
+            # fallback schema, that accepts everything
+            this_params_json_schema = {}
+        properties[param.name] = this_params_json_schema
+
+        # check if param is required
+        if param.default == inspect._empty:
+            required.append(param.name)
+
+    return {
+        "type": "object",
+        "properties": properties,
+        "required": required,
+    }
 
 
 class FunctionTool(BaseTool):
@@ -35,7 +91,7 @@ class FunctionTool(BaseTool):
     @property
     def parameters_json_schema(self) -> dict[str, Any]:
         """JSON schema for tool parameters."""
-        return {}
+        return function_signature_to_json_schema(self.func)
 
     def __call__(
         self,
---
+++ tests/tools/test_function_tool.py
@@ -0,0 +1,56 @@
+from typing import Any, Sequence
+
+import pytest
+
+from llm_agents_from_scratch.tools.function import (
+    function_signature_to_json_schema,
+)
+
+
+def my_mock_fn_1(
+    param1: int,
+    param2: str = "x",
+    *args: Any,
+    **kwargs: Any,
+) -> bool:
+    return False
+
+
+def my_mock_fn_2(
+    param1: int | None,
+    param2: Sequence[int],
+) -> str:
+    return ""
+
+
+@pytest.mark.parametrize(
+    ("func", "properties", "required"),
+    [
+        (
+            my_mock_fn_1,
+            {
+                "param1": {
+                    "type": "number",
+                },
+                "param2": {
+                    "type": "string",
+                },
+            },
+            ["param1"],
+        ),
+        (
+            my_mock_fn_2,
+            {
+                "param1": {},
+                "param2": {},
+            },
+            ["param1", "param2"],
+        ),
+    ],
+)
+def test_function_as_json_schema(func, properties, required) -> None:
+    schema = function_signature_to_json_schema(func)
+
+    assert schema["type"] == "object"
+    assert schema["properties"] == properties
+    assert schema["required"] == required

bb543c78dc9a8493f7af3f01201db531ce644558
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-06-30 17:58:57 -0400

    [chore] Rename to SimpleFunctionTool (#19)
    * renmae to SimpleFunctionTool
    
    * changelog; save changes

--- tests/tools/test_function_tool.py
+++ tests/tools/test_function_tool.py
@@ -4,8 +4,8 @@ from unittest.mock import MagicMock, patch
 import pytest
 
 from llm_agents_from_scratch.data_structures.tool import ToolCall
-from llm_agents_from_scratch.tools.function import (
-    FunctionTool,
+from llm_agents_from_scratch.tools.simple_function import (
+    SimpleFunctionTool,
     function_signature_to_json_schema,
 )
 
@@ -60,8 +60,8 @@ def test_function_as_json_schema(func, properties, required) -> None:
 
 
 def test_function_tool_init() -> None:
-    """Tests FunctionTool initialization."""
-    tool = FunctionTool(my_mock_fn_1, desc="mock desc")
+    """Tests SimpleFunctionTool initialization."""
+    tool = SimpleFunctionTool(my_mock_fn_1, desc="mock desc")
 
     assert tool.name == "my_mock_fn_1"
     assert tool.description == "mock desc"
@@ -71,10 +71,10 @@ def test_function_tool_init() -> None:
     assert tool.func == my_mock_fn_1
 
 
-@patch("llm_agents_from_scratch.tools.function.validate")
+@patch("llm_agents_from_scratch.tools.simple_function.validate")
 def test_function_tool_call(mock_validate: MagicMock) -> None:
     """Tests a function tool call."""
-    tool = FunctionTool(my_mock_fn_1, desc="mock desc")
+    tool = SimpleFunctionTool(my_mock_fn_1, desc="mock desc")
     tool_call = ToolCall(
         tool_name="my_mock_fn_1",
         arguments={"param1": 1, "param2": "y"},
@@ -92,7 +92,7 @@ def test_function_tool_call(mock_validate: MagicMock) -> None:
 
 def test_function_tool_call_returns_error() -> None:
     """Tests a function tool call."""
-    tool = FunctionTool(my_mock_fn_1, desc="mock desc")
+    tool = SimpleFunctionTool(my_mock_fn_1, desc="mock desc")
     tool_call = ToolCall(
         tool_name="my_mock_fn_1",
         arguments={"param1": "1", "param2": "y"},

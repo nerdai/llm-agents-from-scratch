a0a17a7ba01b5aa9a25b683cee07a0b2f467ac95
nerdai <andrei@nerdai.io>
2025-07-06 14:13:53 -0400

    improved import patterns

--- src/llm_agents_from_scratch/tools/__init__.py
+++ src/llm_agents_from_scratch/tools/__init__.py
@@ -1,3 +1,11 @@
-from .simple_function import SimpleFunctionTool
+from .pydantic_function import AsyncPydanticFunctionTool, PydanticFunctionTool
+from .simple_function import AsyncSimpleFunctionTool, SimpleFunctionTool
 
-__all__ = ["SimpleFunctionTool"]
+__all__ = [
+    # simple
+    "AsyncSimpleFunctionTool",
+    "SimpleFunctionTool",
+    # pydantic
+    "AsyncPydanticFunctionTool",
+    "PydanticFunctionTool",
+]

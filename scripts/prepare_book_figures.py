7265dd375878566a746bf17ebb5663a3c1b5660a
nerdai <andrei@nerdai.io>
2025-10-21 23:35:54 -0400

    wip

--- scripts/prepare_book_figures.py
+++ scripts/prepare_book_figures.py
@@ -21,15 +21,23 @@ def _get_image_id(chapter: str, fig_num: str) -> str:
     return f"{chapter.title()}_F{fig_num}_Fajardo"
 
 
-def main(figures_path: Path | str, config_path: Path | None = None) -> None:
+def main(
+    figures_path: Path | str,
+    config_path: Path | None = None,
+    include_svg: bool = False,
+) -> None:
     """Main runner.
 
     Create .png and .svg copies of book figures for every chapter using the
     naming convention as specified by Manning.
 
     Args:
-        figures_path (Path): _description_
-        config_path (Path | None, optional): _description_. Defaults to None.
+        figures_path (Path | str): Path to where book figures are located.
+        config_path (Path | None, optional): Path to where `book_figures.yml`
+        include_svg (bool): Whether or not to include .svg version of images
+
+    Examples:
+        >>> uv run python scripts/prepare_book_figures.py --figures_path "./"
     """
     config_path = config_path or DEFAULT_CONFIG_PATH
 
@@ -45,6 +53,7 @@ def main(figures_path: Path | str, config_path: Path | None = None) -> None:
     cp_commands = []
     print(cfg)
     for ch, data in cfg.items():
+        # png
         cp_commands.extend(
             [
                 "cp",
@@ -53,6 +62,18 @@ def main(figures_path: Path | str, config_path: Path | None = None) -> None:
             ]
             for fig_num, fig_name in data["figures"].items()
         )
+
+        # svg
+        if include_svg:
+            cp_commands.extend(
+                [
+                    "cp",
+                    f"{(figures_path / ch / fig_name).as_posix()}.svg",
+                    f"{output_path / _get_image_id(ch, fig_num)}.svg",
+                ]
+                for fig_num, fig_name in data["figures"].items()
+                if fig_name not in data["png_only"]
+            )
 
     for cmd in cp_commands:
         subprocess.run(cmd, check=False)

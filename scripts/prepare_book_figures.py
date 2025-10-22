ac3950ea7f4714247b575683a4993bd1262c7d87
Andrei Fajardo <92402603+nerdai@users.noreply.github.com>
2025-10-21 23:41:11 -0400

    feat: wrap up script for preparing manning figures by adding cp commands (#237)
    * add cp commands
    
    * wip
    
    * rm debug statements

--- scripts/prepare_book_figures.py
+++ scripts/prepare_book_figures.py
@@ -5,6 +5,7 @@ figures can be found, and creates copies of each figure, using the naming
 convention required for Manning books.
 """
 
+import subprocess
 from pathlib import Path
 
 import fire
@@ -14,22 +15,67 @@ FIGURES_YAML_FILENAME = "book_figures.yml"
 DEFAULT_CONFIG_PATH = Path(__file__).parents[1].absolute()
 
 
-def main(figures_path: Path, config_path: Path | None = None) -> None:
+def _get_image_id(chapter: str, fig_num: str) -> str:
+    if int(fig_num) < 10:  # noqa: PLR2004
+        return f"{chapter.title()}_F0{fig_num}_Fajardo"
+    return f"{chapter.title()}_F{fig_num}_Fajardo"
+
+
+def main(
+    figures_path: Path | str,
+    config_path: Path | None = None,
+    include_svg: bool = False,
+) -> None:
     """Main runner.
 
     Create .png and .svg copies of book figures for every chapter using the
     naming convention as specified by Manning.
 
     Args:
-        figures_path (Path): _description_
-        config_path (Path | None, optional): _description_. Defaults to None.
+        figures_path (Path | str): Path to where book figures are located.
+        config_path (Path | None, optional): Path to where `book_figures.yml`
+        include_svg (bool): Whether or not to include .svg version of images
+
+    Examples:
+        >>> uv run python scripts/prepare_book_figures.py --figures_path "./"
     """
     config_path = config_path or DEFAULT_CONFIG_PATH
 
     with open(config_path / FIGURES_YAML_FILENAME, "r") as f:
         cfg = yaml.safe_load(f)
 
-    print(cfg)
+    if isinstance(figures_path, str):
+        figures_path = Path(figures_path)
+
+    # loop through all figures
+    output_path = figures_path / "all_chapters"
+    output_path.mkdir(exist_ok=True)
+    cp_commands = []
+    for ch, data in cfg.items():
+        # png
+        cp_commands.extend(
+            [
+                "cp",
+                f"{(figures_path / ch / fig_name).as_posix()}.png",
+                f"{output_path / _get_image_id(ch, fig_num)}.png",
+            ]
+            for fig_num, fig_name in data["figures"].items()
+        )
+
+        # svg
+        if include_svg:
+            cp_commands.extend(
+                [
+                    "cp",
+                    f"{(figures_path / ch / fig_name).as_posix()}.svg",
+                    f"{output_path / _get_image_id(ch, fig_num)}.svg",
+                ]
+                for fig_num, fig_name in data["figures"].items()
+                if fig_name not in data["png_only"]
+            )
+
+    for cmd in cp_commands:
+        subprocess.run(cmd, check=False)
 
 
 if __name__ == "__main__":

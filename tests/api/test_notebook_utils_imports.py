19790a058f46318cd5c590b3957edd86b7c2d5a6
nerdai <andrei@nerdai.io>
2025-12-06 20:38:56 -0500

    set_dataframe_display_options to notebook_utils api

---
+++ tests/api/test_notebook_utils_imports.py
@@ -0,0 +1,17 @@
+"""Unit tests for importing notebook utils."""
+
+import importlib
+
+import pytest
+
+from llm_agents_from_scratch import notebook_utils
+
+
+@pytest.mark.parametrize("name", notebook_utils.__all__)
+def test_public_generators_all_importable(name: str) -> None:
+    """Tests that all names listed in notebook_utils __all__ are importable."""
+    mod = importlib.import_module("llm_agents_from_scratch.notebook_utils")
+    attr = getattr(mod, name)
+
+    assert hasattr(mod, name)
+    assert attr is not None

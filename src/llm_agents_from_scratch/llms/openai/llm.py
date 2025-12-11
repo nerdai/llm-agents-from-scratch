e5419566b96dc535486417108b85e306adf1f9c5
nerdai <andrei@nerdai.io>
2025-12-11 07:15:45 -0500

    implement structured output

--- src/llm_agents_from_scratch/llms/openai/llm.py
+++ src/llm_agents_from_scratch/llms/openai/llm.py
@@ -2,12 +2,12 @@
 
 from typing import Any
 
-from llm_agents_from_scratch.base.llm import BaseLLM
+from llm_agents_from_scratch.base.llm import LLM, StructuredOutputType
 from llm_agents_from_scratch.data_structures.llm import CompleteResult
 from llm_agents_from_scratch.utils import check_extra_was_installed
 
 
-class OpenAILLM(BaseLLM):
+class OpenAILLM(LLM):
     """OpenAI LLM integration."""
 
     def __init__(
@@ -35,10 +35,35 @@ class OpenAILLM(BaseLLM):
         self.client = AsyncOpenAI(api_key=api_key)
 
     async def complete(self, prompt: str, **kwargs: Any) -> CompleteResult:
-        """Implements complete."""
+        """Implements complete LLM interaction mode."""
         response = await self.client.responses.create(
             model=self.model,
             input=prompt,
             **kwargs,
         )
         return CompleteResult(response=response, prompt=prompt)
+
+    async def structured_output(
+        self,
+        prompt: str,
+        mdl: type[StructuredOutputType],
+        **kwargs: Any,
+    ) -> StructuredOutputType:
+        """Implements structured output LLM interaction mode.
+
+        Args:
+            prompt (str): The prompt to elicit the structured output response.
+            mdl (type[StructuredOutputType]): The ~pydantic.BaseModel to output.
+            **kwargs (Any): Additional keyword arguments.
+
+        Returns:
+            StructuredOutputType: The structured output as the specified `mdl`
+                type.
+        """
+        response = await self.client.responses.parse(
+            model=self.model,
+            input=prompt,
+            text_format=mdl,
+            **kwargs,
+        )
+        return response.output_parsed  # type: ignore[no-any-return]

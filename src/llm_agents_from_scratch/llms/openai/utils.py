a70dc549a253c201fcfe2ab53bb02c1e23da2556
nerdai <andrei@nerdai.io>
2025-12-19 01:46:12 -0500

    implement openai_response_to_chat_message

--- src/llm_agents_from_scratch/llms/openai/utils.py
+++ src/llm_agents_from_scratch/llms/openai/utils.py
@@ -1,9 +1,11 @@
 """Converters for OpenAI Responses API."""
 
+import json
 from typing import TYPE_CHECKING
 
 from llm_agents_from_scratch.base.tool import Tool
-from llm_agents_from_scratch.data_structures.llm import ChatMessage
+from llm_agents_from_scratch.data_structures.llm import ChatMessage, ChatRole
+from llm_agents_from_scratch.data_structures.tool import ToolCall
 
 if TYPE_CHECKING:  # pragma: no cover
     from openai.types.responses import (
@@ -15,7 +17,24 @@ if TYPE_CHECKING:  # pragma: no cover
 
 def openai_response_to_chat_message(openai_response: "Response") -> ChatMessage:
     """Convert an ~openai.Response to ChatMessage."""
-    pass
+    content = openai_response.output_text
+    role = ChatRole.ASSISTANT
+
+    # collect tool calls
+    tool_calls = []
+    for item in openai_response.output:
+        if item.type != "function_call":
+            continue
+
+        tool_calls.append(
+            ToolCall(
+                id_=item.call_id,
+                tool_name=item.name,
+                arguments=json.loads(item.arguments),
+            ),
+        )
+
+    return ChatMessage(content=content, role=role, tool_calls=tool_calls)
 
 
 def chat_message_to_openai_response_input_param(

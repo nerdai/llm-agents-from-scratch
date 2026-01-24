15a502ba65fbedf609116ef948698661e4bbc062
nerdai <andrei@nerdai.io>
2026-01-24 04:44:57 -0500

    simple hailstone mcp

--- extra/mcp-hailstone/main.py
+++ extra/mcp-hailstone/main.py
@@ -1,9 +1,18 @@
 """Main application."""
 
+from mcp.server.fastmcp import FastMCP
 
-def _main():
-    print("Hello from mcp-hailstone!")
+# Create an MCP server
+mcp = FastMCP("Hailstone")
+
+
+@mcp.tool()
+async def hailstone_step_fn(x: int) -> int:
+    """Performs a single step of the Hailstone sequence."""
+    if x % 2 == 0:
+        return x // 2
+    return 3 * x + 1
 
 
 if __name__ == "__main__":
-    _main()
+    mcp.run(transport="stdio")  # pragma: no cover
